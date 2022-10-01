d7bdb73559e91c9d7458e4adadfed39ea8b81925
rabinKarkiWeb <rabinkarkiweb@gmail.com>
2022-10-01 13:48:55 +0545

    finish styling react

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
+import styles from './Button.module.css';
 
-import './Button.css';
-
+// const Button =styled.button`
+//
+//     font: inherit;
+//     padding: 0.5rem 1.5rem;
+//     border: 1px solid #1baaa8;
+//     color: white;
+//     background: #1baaa8;
+//     box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//     cursor: pointer;
+//
+//
+//   &:focus {
+//     outline: none;
+//   }
+//
+//   &:hover,
+//   &:active {
+//     background: #178b8a;
+//     border-color: #178b8a;
+//     box-shadow: 0 0 8px rgba(23, 139, 138, 0.25);
+//   }
+//
+//
+// `
 const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
